fb0756ea26497a86cb1f8eae4caa6215a4262ba5
Byron Mejia <byron.mejia@connect.qut.edu.au>
2017-03-08 21:32:22 +1000

    order_details may belong to a campaign

--- migrations/20170308203836_create_order_details.js
+++ migrations/20170308203836_create_order_details.js
@@ -1,5 +1,6 @@
 const TABLE_NAME = 'order_details';
 const orderTable = require('./20170308203005_create_orders').TABLE_NAME;
+const campaignTable = require('./20170308202600_create_campaign').TABLE_NAME;
 
 exports.TABLE_NAME = TABLE_NAME;
 
@@ -8,6 +9,7 @@ exports.up = function(knex, Promise) {
     knex.schema.createTable(TABLE_NAME, function(table){
       table.integer('id').primary();
       table.integer('order_id').references('id').inTable(orderTable).notNullable();
+      table.integer('campaign_id').references('id').inTable(campaignTable);
       table.string('title').notNullable();
       table.string('description').notNullable();
       table.integer('quantity').notNullable();
