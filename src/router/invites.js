e53459d20470e4e72ac34bb7ae8f5318c0e85b8b
Byron Mejia <byron.mejia@connect.qut.edu.au>
2017-03-08 19:22:41 +1000

    We documented now

--- src/router/invites.js
+++ src/router/invites.js
@@ -4,6 +4,25 @@ import interactor from '../interactor/invites';
 
 const router = new Router();
 
+/**
+ * @api {post} /invites/slack Invite someone to Slack via Email
+ * @apiName InviteToSlack
+ * @apiGroup Invites
+ *
+ * @apiParam {String} email Invitee's Email.
+ *
+ * @apiSuccess {Boolean} ok Boolean of successful call.
+ * @apiSuccess {String} msg Developer friendly message.
+ * @apiSuccess {String} email The email of the invitee.
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *   HTTP/1.1 200 OK
+ *   {
+ *      "ok": true,
+ *      "msg": "Email has been invited",
+ *      "email": "example@codenetwork.co"
+ *   }
+ */
 router.post('/slack',
   (req, res) => {
     const { input, response } = adapter.inviteSlackExpressAdapter(req, res);
