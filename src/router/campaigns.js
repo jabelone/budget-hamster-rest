7ed66e35c5ce452d288baf2004d239aec9e52d5b
Byron Mejia <byron.mejia@connect.qut.edu.au>
2017-03-09 08:25:44 +1000

    Preparing campaign crud

--- src/router/campaigns.js
+++ src/router/campaigns.js
@@ -1,5 +1,42 @@
 import { Router } from 'express';
+import adapter from '../adapter/ca';
+import interactor from '../interactor/invites';
 
 const router = new Router();
+
+// Get all
+router.get('/',
+  (req, res) => {
+    throw new Error('Not yet implemented');
+  },
+);
+
+// Create One
+router.post('/',
+  (req, res) => {
+    throw new Error('Not yet implemented');
+  },
+);
+
+// Read One
+router.get('/:id',
+  (req, res) => {
+    throw new Error('Not yet implemented');
+  },
+);
+
+// Update One
+router.put('/:id',
+  (req, res) => {
+    throw new Error('Not yet implemented');
+  },
+);
+
+// Destroy One
+router.delete('/:id',
+  (req, res) => {
+    throw new Error('Not yet implemented');
+  },
+);
 
 export default router;
